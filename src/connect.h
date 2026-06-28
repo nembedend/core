d30d8a38da6101457bf4a9c1472c1083f33f9167
Sergey Chupligin <neochapay@gmail.com>
2026-06-28 12:11:58 +0300

    Make some order

--- src/connect.h
+++ src/connect.h
@@ -1,3 +1,10 @@
+// Copyright (c) 2026 Chupligin Sergey
+//
+// This project is licensed under the GNU General Public License v3.0.
+//
+// Commercial licenses are available for proprietary and closed-source products.
+// See COMMERCIAL_LICENSE.md for details.
+
 #pragma once
 
 #include "nsignal.h"
